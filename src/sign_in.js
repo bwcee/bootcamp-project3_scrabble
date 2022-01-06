8d9451111253bb5e9005343bf970615bbd822c63
bwcee <bwceemail@gmail.com>
2022-01-06 09:51:36 +0800

    added sign-up div w/o function

--- src/sign_in.js
+++ src/sign_in.js
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////
 /* create the initial sign-in page */
 document
-  .getElementById("main")
+  .getElementById("body")
   .insertAdjacentHTML(
     "afterbegin",
     '<div id="signInDiv" class="form-row d-flex justify-content-center">' +
@@ -27,12 +27,39 @@ document
 2. div starts out w .hide
  */
 document
-  .getElementById("main")
+  .getElementById("body")
   .insertAdjacentHTML(
-    "beforeend",
+    "afterbegin",
     '<div id="selectGameDiv" class="hide text-center">' +
-      '<button id="newGameBtn" class="btn btn-success d-sm-block">Hit me with a new game!!</button>' +
+      '<button id="newGameBtn" class="btn btn-success">Hit me with a new game!!</button><br>' +
       '<button id="logOutBtn" class="btn btn-danger mt-3">Log me out!!</button>' +
+      "</div>"
+  );
+
+/* 
+1. sign-up
+2. div starts out w .hide
+ */
+document
+  .getElementById("body")
+  .insertAdjacentHTML(
+    "afterbegin",
+    '<div id="signUpDiv" class="justify-content-center hide">' +
+      '<div class="form row mb-2">' +
+      '<label class="col-sm-2" for="reg_name">Name: </label>' +
+      '<input type="text" id="reg_name" name="reg_name" class="form-control col-sm-10" placeholder="prease input name good person" required=true>' +
+      "</div>" +
+      '<div class="form row mb-2">' +
+      '<label class="col-sm-2" for="reg_email">Email: </label>' +
+      '<input type="email" id="reg_email" name="reg_email" class="form-control col-sm-10" placeholder="prease input email good person" required=true>' +
+      "</div>" +
+      '<div class="form row mb-2">' +
+      '<label class="col-sm-2" for="reg_password">Password: </label>' +
+      '<input type="password" id="reg_password" name="reg_password" class="form-control col-sm-10" placeholder="prease input password good person" required=true>' +
+      "</div>" +
+      '<div id="btnGrp" class="form-group col-md-12 text-center">' +
+      '<button id="registerBtn" class="btn btn-success mt-2 mx-3">Register now!!</button>' +
+      "</div>" +
       "</div>"
   );
 
@@ -87,6 +114,12 @@ document.getElementById("signInBtn").addEventListener("click", async () => {
     console.log(err);
   }
 });
+
+/* sign in btn functionality */
+document.getElementById("signUpBtn").addEventListener("click", async () => {
+  document.getElementById("signInDiv").remove();
+  document.getElementById("signUpDiv").classList.toggle("hide");
+})
 
 /* log out btn functionality */
 logOutBtn.addEventListener("click", () => {
